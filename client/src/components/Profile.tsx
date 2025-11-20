7bf74aef94d0ad1e879b0c5a6cc1cbe0c1cb1ed2
Doan Cong Dat <doancongdat2004@gmail.com>
2025-11-20 21:13:26 +0700

    done HeroSection page

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -1,7 +1,9 @@
 import { useRef, useState } from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
-import { Plus } from 'lucide-react'
+import { Loader2, LocateIcon, Mail, MapPin, MapPinnedIcon, Plus } from 'lucide-react'
 import { Input } from './ui/input'
+import { Label } from './ui/label'
+import { Button } from './ui/button'
 
 export default function Profile() {
   const [isLoading, setIsLoading] = useState<boolean>(false)
@@ -36,6 +38,69 @@ export default function Profile() {
             className='font-bold text-2xl outline-none border-none focus-visible:ring-transparent'
           />
         </div>
+      </div>
+      <div className='grid md:grid-cols-4 md:gap-2 gap-3 my-10'>
+        <div className='flex items-center gap-4 rounded-sm p-2 bg-gray-200'>
+          <Mail className='text-gray-500' />
+          <div className='w-full'>
+            <Label>Email</Label>
+            <input
+              // disabled
+              name='email'
+              // value={profileData.email}
+              // onChange={changeHandler}
+              className='w-full text-gray-600 bg-transparent focus-visible:ring-0 focus-visible:border-transparent outline-none border-none'
+            />
+          </div>
+        </div>
+        <div className='flex items-center gap-4 rounded-sm p-2 bg-gray-200'>
+          <LocateIcon className='text-gray-500' />
+          <div className='w-full'>
+            <Label>Address</Label>
+            <input
+              name='address'
+              // value={profileData.address}
+              // onChange={changeHandler}
+              className='w-full text-gray-600 bg-transparent focus-visible:ring-0 focus-visible:border-transparent outline-none border-none'
+            />
+          </div>
+        </div>
+        <div className='flex items-center gap-4 rounded-sm p-2 bg-gray-200'>
+          <MapPin className='text-gray-500' />
+          <div className='w-full'>
+            <Label>City</Label>
+            <input
+              name='city'
+              // value={profileData.city}
+              // onChange={changeHandler}
+              className='w-full text-gray-600 bg-transparent focus-visible:ring-0 focus-visible:border-transparent outline-none border-none'
+            />
+          </div>
+        </div>
+        <div className='flex items-center gap-4 rounded-sm p-2 bg-gray-200'>
+          <MapPinnedIcon className='text-gray-500' />
+          <div className='w-full'>
+            <Label>Country</Label>
+            <input
+              name='country'
+              // value={profileData.country}
+              // onChange={changeHandler}
+              className='w-full text-gray-600 bg-transparent focus-visible:ring-0 focus-visible:border-transparent outline-none border-none'
+            />
+          </div>
+        </div>
+      </div>
+      <div className='text-center'>
+        {isLoading ? (
+          <Button disabled className='bg-orange hover:bg-hoverOrange'>
+            <Loader2 className='mr-2 w-4 h-4 animate-spin' />
+            Please wait
+          </Button>
+        ) : (
+          <Button type='submit' className='bg-orange hover:bg-hoverOrange'>
+            Update
+          </Button>
+        )}
       </div>
     </form>
   )
